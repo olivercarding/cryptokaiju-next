a2cffec1b04450685002ed7a757ecc71bf3cdee4
Oliver Carding <ocarding@ebay.com>
2025-07-13 19:21:51 +0100

    GTM

--- src/components/analytics/GoogleTagManager.tsx
+++ src/components/analytics/GoogleTagManager.tsx
@@ -22,22 +22,19 @@ export function GoogleTagManager({ gtmId }: GTMProps) {
   }, [])
 
   return (
-    <>
-      {/* Google Tag Manager Script */}
-      <Script
-        id="gtm-script"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','${gtmId}');
-          `,
-        }}
-      />
-    </>
+    <Script
+      id="gtm-script"
+      strategy="afterInteractive"
+      dangerouslySetInnerHTML={{
+        __html: `
+          (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','${gtmId}');
+        `,
+      }}
+    />
   )
 }
 
@@ -54,7 +51,7 @@ export function GoogleTagManagerNoScript({ gtmId }: GTMProps) {
   )
 }
 
-// Custom event tracking functions
+// Simple event tracking function for custom events
 export const gtmEvent = (eventName: string, parameters?: Record<string, any>) => {
   if (typeof window !== 'undefined' && window.dataLayer) {
     window.dataLayer.push({
@@ -62,61 +59,4 @@ export const gtmEvent = (eventName: string, parameters?: Record<string, any>) =>
       ...parameters,
     })
   }
-}
-
-// Specific event tracking functions for your CryptoKaiju site
-export const trackMintEvent = (tokenId: string, nfcId: string, priceETH: number) => {
-  gtmEvent('mint_nft', {
-    event_category: 'NFT',
-    event_label: 'CryptoKaiju Mint',
-    token_id: tokenId,
-    nfc_id: nfcId,
-    price_eth: priceETH,
-    currency: 'ETH'
-  })
-}
-
-export const trackWalletConnection = (walletType: string) => {
-  gtmEvent('wallet_connect', {
-    event_category: 'Web3',
-    event_label: 'Wallet Connected',
-    wallet_type: walletType
-  })
-}
-
-export const trackPageView = (pageName: string, pageTitle?: string) => {
-  gtmEvent('page_view', {
-    page_title: pageTitle || document.title,
-    page_location: window.location.href,
-    page_name: pageName
-  })
-}
-
-export const trackKaijuView = (tokenId: string, kaijuName?: string) => {
-  gtmEvent('view_item', {
-    event_category: 'NFT',
-    event_label: 'Kaiju Viewed',
-    item_id: tokenId,
-    item_name: kaijuName || `CryptoKaiju #${tokenId}`,
-    item_category: 'CryptoKaiju'
-  })
-}
-
-export const trackShippingStart = (orderNumber: string, itemCount: number) => {
-  gtmEvent('begin_checkout', {
-    event_category: 'Physical Fulfillment',
-    event_label: 'Shipping Started',
-    order_number: orderNumber,
-    items: itemCount
-  })
-}
-
-export const trackOrderComplete = (orderNumber: string, shippingCost: number, currency: string) => {
-  gtmEvent('purchase', {
-    event_category: 'Physical Fulfillment',
-    event_label: 'Order Completed',
-    transaction_id: orderNumber,
-    value: shippingCost,
-    currency: currency
-  })
 }
--- src/lib/hooks/useGTM.ts
+++ src/lib/hooks/useGTM.ts
@@ -3,15 +3,7 @@
 
 import { useEffect } from 'react'
 import { usePathname } from 'next/navigation'
-import { 
-  gtmEvent, 
-  trackMintEvent, 
-  trackWalletConnection, 
-  trackPageView, 
-  trackKaijuView,
-  trackShippingStart,
-  trackOrderComplete
-} from '@/components/analytics/GoogleTagManager'
+import { gtmEvent } from '@/components/analytics/GoogleTagManager'
 
 export function useGTM() {
   const pathname = usePathname()
@@ -19,88 +11,42 @@ export function useGTM() {
   // Track page views automatically
   useEffect(() => {
     const pageName = pathname === '/' ? 'Home' : pathname.replace('/', '').replace('-', ' ')
-    trackPageView(pageName)
+    
+    // Send page view event
+    gtmEvent('page_view', {
+      page_title: document.title,
+      page_location: window.location.href,
+      page_path: pathname,
+      page_name: pageName
+    })
   }, [pathname])
 
   return {
     // Generic event tracking
     trackEvent: gtmEvent,
     
-    // Specific CryptoKaiju events
-    trackMint: trackMintEvent,
-    trackWalletConnect: trackWalletConnection,
-    trackKaijuView: trackKaijuView,
-    trackShippingStart: trackShippingStart,
-    trackOrderComplete: trackOrderComplete,
-    
-    // Custom events for your specific use cases
-    trackMysteryBoxOpen: (boxNumber: number, result: string) => {
-      gtmEvent('mystery_box_open', {
-        event_category: 'Engagement',
-        event_label: 'Mystery Box Opened',
-        box_number: boxNumber,
-        result: result
-      })
-    },
-    
-    trackNFTSearch: (searchTerm: string, resultsCount: number) => {
-      gtmEvent('search', {
-        search_term: searchTerm,
-        event_category: 'Search',
-        event_label: 'NFT Search',
-        results_count: resultsCount
-      })
-    },
-    
-    trackCollectionView: (collectionType: string) => {
-      gtmEvent('view_item_list', {
-        event_category: 'Collection',
-        event_label: 'Collection Viewed',
-        item_list_name: collectionType
-      })
-    },
-    
-    trackSocialShare: (platform: string, content: string) => {
-      gtmEvent('share', {
-        event_category: 'Social',
-        event_label: 'Content Shared',
-        method: platform,
-        content_type: content
-      })
-    },
-    
-    trackNewsletterSignup: (source: string) => {
-      gtmEvent('newsletter_signup', {
-        event_category: 'Lead Generation',
-        event_label: 'Newsletter Signup',
-        source: source
-      })
-    },
-    
-    trackVideoPlay: (videoName: string, progress: number) => {
-      gtmEvent('video_play', {
-        event_category: 'Engagement',
-        event_label: 'Video Played',
-        video_title: videoName,
-        video_progress: progress
-      })
-    },
-    
-    trackExternalLinkClick: (url: string, linkText: string) => {
+    // Helper for common events
+    trackClick: (elementName: string, additional?: Record<string, any>) => {
       gtmEvent('click', {
-        event_category: 'External Link',
-        event_label: 'External Link Clicked',
-        link_url: url,
-        link_text: linkText
+        event_category: 'UI',
+        event_label: elementName,
+        ...additional
       })
     },
     
-    trackErrorEvent: (errorType: string, errorMessage: string, page: string) => {
+    trackFormSubmit: (formName: string, additional?: Record<string, any>) => {
+      gtmEvent('form_submit', {
+        event_category: 'Form',
+        event_label: formName,
+        ...additional
+      })
+    },
+    
+    trackError: (errorMessage: string, errorType?: string) => {
       gtmEvent('exception', {
         description: errorMessage,
         fatal: false,
-        error_type: errorType,
-        page: page
+        error_type: errorType || 'javascript_error'
       })
     }
   }
